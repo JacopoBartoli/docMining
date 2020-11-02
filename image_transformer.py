637c2a0b2bcb9d604536415972ad3a5fc06f1a0c
JacopoBartoli <bartolijacopo@gmail.com>
2020-11-02 14:15:06 +0100

    Update image_transformer.py. The function image_tranformation seems to work right. Added comment to explain the function and possible change/tuning on the code.

--- image_transformer.py
+++ image_transformer.py
@@ -1,15 +1,36 @@
 import numpy as np
 import cv2
-from scipy.ndimage import distance_transform_edt
 
 
 def image_transformation(img):
     # Transform the image utilizing distance transform.
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Check if the threshold is right. (254)
+    (thresh, bn_img) = cv2.threshold(gray_img, 254, 255, cv2.THRESH_BINARY)
 
     # The metric used for distance is euclidean
-    blue_channel = distance_transform_edt(img)
-    # Linear distance transform !!!
-    green_channel = None
-    # Max distance transform
-    red_channel = None
+    blue_channel = cv2.distanceTransform(bn_img, distanceType=cv2.DIST_L2, maskSize=5)
+    # Linear distance transform. This is L1 norm. I suppose this is the right distance
+    green_channel = cv2.distanceTransform(bn_img, distanceType=cv2.DIST_L1, maskSize=3)
+    # Max distance transform. max(|x1-x2|,|y1-y2|)
+    red_channel = cv2.distanceTransform(bn_img, distanceType=cv2.DIST_C, maskSize=3)
+
+    # Merge the three channel
     transformed = cv2.merge((blue_channel, green_channel, red_channel))
+    # Make the image in gray scale again.
+    transformed = cv2.cvtColor(transformed, cv2.COLOR_BGR2GRAY)
+    # Convert the image in the right format. Only if adaptive threshold is used.
+    # transformed = np.uint8(transformed)
+
+    # The first value of the threshold should be adapted.
+    # We can try to use cv2.THRESH_OTSU. Need to read documentation first
+    ret, transformed = cv2.threshold(transformed, 15, 255, cv2.THRESH_BINARY)
+
+    # Adaptive threshold seems to not be the right answer at this threshold problem.
+    #transformed = cv2.adaptiveThreshold(transformed, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
+
+    return transformed
+
+if __name__ == '__main__':
+    image = cv2.imread('./Dataset/icdar/images/POD_0067.jpg')
+    trs = image_transformation(image)
