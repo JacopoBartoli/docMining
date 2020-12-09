4b5fdc3fb586e5e33094eacb1efe29b77b185f28
JacopoBartoli <bartolijacopo@gmail.com>
2020-12-09 11:00:10 +0100

    MChanges in utils.py

--- utils.py
+++ utils.py
@@ -35,8 +35,10 @@ def create_rectangle_marmot(points, img_width=1, img_height=1):
     # Points[3][0] refer to the x coord of the bottom right angle.
     width = points[1, 0] - points[0, 0]
     height = points[2, 1] - points[0, 1]
-    x_center = (points[1, 0] - points[0, 0]) / 2.0
-    y_center = (points[2, 1] - points[0, 1]) / 2.0
+
+    x_center = points[0, 0] + width / 2.0
+    y_center = points[0, 1] + height / 2.0
+
     return Rectangle(width / img_width, height / img_height, x_center / img_width, y_center / img_height)
 
 
@@ -159,51 +161,6 @@ def save_classes(classes, path):
     for key in classes.keys():
         file.write(str(key) + '\n')
     file.close()
-
-
-# # Convert the ICDAR 2017 POD dataset into the darknet format.
-# OBSOLETE representation, the not binary one.
-# def icdar_to_darknet():
-#     # Pass the path as parameter here too?
-#
-#     # Path of input annotations.
-#     in_annotations_dir = "./Dataset/icdar_2017/Annotations"
-#     # Output path for the new annotations.
-#     out_annotations_dir = "./Dataset/icdar/labels"
-#     # Path of input images.
-#     in_image_dir = "./Dataset/icdar_2017/Images"
-#     # Output path for the new images.
-#     out_image_dir = "./Dataset/icdar/images"
-#
-#     # Get the dictionary with all the classes
-#     classes_dir = "./Dataset/icdar.names"
-#     class_dict = get_classes(in_annotations_dir)
-#     print(class_dict)
-#     save_classes(class_dict, classes_dir)
-#
-#     for filename in os.listdir(in_annotations_dir):
-#         tree = et.parse(in_annotations_dir + '/' + filename)
-#         root = tree.getroot()
-#         # Storing the corresponding image to the file.
-#         img_filename = in_image_dir + '/' + filename.replace('.xml', '.bmp')
-#         img = Image.open(img_filename)
-#         if len(root.items()) != 0:
-#             out_annotation = open(out_annotations_dir + '/' + filename.replace('.xml', '.txt'), "w+")
-#         for child in root:
-#             for item in child:
-#                 if item.tag == 'Coords':
-#                     img_width, img_height = img.size
-#                     normalized_box = calc_box(item, img_width, img_height)
-#                     # Create new annotation files and save them in the right directory, and in the right format(.txt).
-#                     out_annotation.write(str(class_dict[child.tag]) + ' ')
-#                     out_annotation.write(str(normalized_box.x_center) + ' ')
-#                     out_annotation.write(str(normalized_box.y_center) + ' ')
-#                     out_annotation.write(str(normalized_box.width) + ' ')
-#                     out_annotation.write(str(normalized_box.height) + '\n')
-#
-#                     # Create new image files and save them in the right directory, and in the right format(.jpg)
-#         img.save(out_image_dir + '/' + filename.replace('.xml', '.jpg'))
-#         out_annotation.close()
 
 
 def convert_marmot(in_annotations_dir, in_image_dir, out_annotations_dir, out_image_dir, classes_dir):
